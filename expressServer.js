5b5abcd8783100c8de51af2c94f0bd612cf6abee
AMANDA RUTHERFOORD <AMANDARUTH08@GMAIL.COM>
2018-06-13 11:37:41 -0500

    completed without bonus

---
+++ expressServer.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let petsPath = path.join(__dirname, 'pets.json');
+
+let express = require('express'); //creates an express application
+let app = express(); //express function is a top-evel function exported by the expression module
+let port = process.env.PORT || 8000; // sets the environment variable to port which tells out server where to listen on
+
+app.disable('x-powered-by');
+
+app.get('/pets', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    let pets = JSON.parse(petsJSON);
+
+    res.send(pets);
+  });
+});
+
+app.get('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    let id = Number.parseInt(req.params.id);
+    let pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'application/json');
+    res.send(pets[id]);
+  });
+});
+
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
+module.exports = app;
